c620124a3334139b2a141a446c96f36ce5d5bfe5
jeet soni <37554133+jeetsoni@users.noreply.github.com>
2020-05-20 12:22:19 +0530

    second

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,51 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require('./notes')
 
-const command = process.argv[2]
 
-if(command === 'add') {
-    console.log('Adding Notes');
-} else if(command === 'remove') {
-    console.log('Removing note!');
-}
+yargs.command({
+    command:'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Notes Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log("Removing a note")
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log("Listign all notes")
+    }
+})
+
+yargs.command({
+    command:'read',
+    describe: 'reading all notes',
+    handler: function() {
+        console.log("reading all notes")
+    }
+})
+
+yargs.parse()
