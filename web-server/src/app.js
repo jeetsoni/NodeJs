fccac6a79a6ab0dcf96403d37e4410546a1afa26
jeet soni <37554133+jeetsoni@users.noreply.github.com>
2020-05-22 21:38:35 +0530

    Handle-bars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,39 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../views/partials/')
+
+
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialsPath)
 
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Wether App',
+        name: 'Jeet'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Jeet soni'
+    })
+}) 
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Jeet soni'
+    })
+})
 
 app.get('/weather', (req, res) => {
         res.send([
