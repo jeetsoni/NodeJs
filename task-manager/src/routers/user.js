c12d5767c45b84e9ae9d243d3e2a00159e7f7f29
jeet soni <37554133+jeetsoni@users.noreply.github.com>
2020-05-26 21:14:13 +0530

    auth-middleware

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = new express.Router()
+const auth = require('../middleware/auth')
 const User = require('../models/user')
 
 router.post('/users', async (req,res) => {
     const user  = new User(req.body) 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (e) {
         res.status(400).send(e)
     } 
@@ -15,19 +17,16 @@ router.post('/users', async (req,res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
     } catch (e) {
+        console.log(e)
         res.status(400).send()
     }
 })
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch {
-        res.status(500).send()
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -35,7 +34,7 @@ router.get('/users/:id', async (req, res) => {
     try {
         const user = await User.findById(_id)
         if(!user) {
-            return res.status(404).send()
+            return res.status(404).send( )
         }
         res.send(user)
     } catch {
