a32b08c8e5e133b75b7218d9ab4c8db7c30cda6e
jeet soni <37554133+jeetsoni@users.noreply.github.com>
2020-05-26 11:58:31 +0530

    async

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,10 +1,23 @@
 require('../src/db/mongoose')
 const Tasks = require('../src/models/task')
 
-Tasks.findByIdAndDelete('5ecb957bbadcb0368c63d8f0').then((task) => {
-    console.log(task)
-    return Tasks.countDocuments({completed: false})
-}).then((count) => {
+// Tasks.findByIdAndDelete('5ecb957bbadcb0368c63d8f0').then((task) => {
+//     console.log(task)
+//     return Tasks.countDocuments({completed: false})
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Tasks.findByIdAndDelete(id)
+    const count = await Tasks.countDocuments({completed: false})
+    return count
+}
+
+deleteTaskAndCount('5ecca7370b03084234e516c6').then((count) => {
     console.log(count)
 }).catch((e) => {
     console.log(e)
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,11 +1,23 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5ecbe3ba97b13c6af07f115c', { age:1 }).then((user) => {
-    console.log(user)
-    return User.countDocuments({ age:1 })
-}).then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate('5ecbe3ba97b13c6af07f115c', { age:1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age:1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age: age})
+    const count = await User.countDocuments({ age })
+    return count
+}
+updateAgeAndCount('5ecb8f241e8e08018878867f', 2).then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
-})
+})   
